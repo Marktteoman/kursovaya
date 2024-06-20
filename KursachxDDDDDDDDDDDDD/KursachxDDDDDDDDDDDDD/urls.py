6e41c1922c70f7c77600bde797ef87f10dc71c8f
Mark <toksunteo@mail.ru>
2024-06-20 05:19:10 +0300

    bla bla

--- KursachxDDDDDDDDDDDDD/KursachxDDDDDDDDDDDDD/urls.py
+++ KursachxDDDDDDDDDDDDD/KursachxDDDDDDDDDDDDD/urls.py
@@ -10,3 +10,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("myapp/", include("myapp.urls", namespace="myapp"))
 ] + static(settings.STATIC_URL, dcoument_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
